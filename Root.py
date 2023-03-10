b76b805d3f2a4f8c68e439d42ba584498e950932
eyad45 <123212811+eyad45@users.noreply.github.com>
2023-03-10 10:19:17 +0200

    Create Root.py

---
+++ Root.py
@@ -0,0 +1,13 @@
+#__importing the python sources__
+Import requests 
+
+#__working on it__
+While true:
+   Print("working_-+") 
+#_starting mision__
+
+Class download:
+  Def __init__(self, neep, url) :
+    With open(self.neep)as down:
+      #__saving files__
+      down.get(self.url)
